60bf09f8332fe7eb3b626d32c11589d0829bb511
Ibrahim Nasri <ibrahimnasri24@ubilityai.com>
2023-08-25 13:06:04 +0300

    first commit

---
+++ skeleton/README.md
@@ -0,0 +1,14 @@
+# Next App Starter
+
+## Description
+
+A starter NextJS application using typescript and tailwind css
+
+## Develop
+
+```bash
+# install dependencies
+$ yarn
+# start development server
+$ yarn dev
+```
---
+++ skeleton/components/HelloWorld.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function HelloWorld() {
+  return (
+    <div className="w-full flex justify-center items-center mt-20">
+      <h1 className="text-5xl">Hello World!</h1>
+    </div>
+  );
+}
+
+export default HelloWorld;
